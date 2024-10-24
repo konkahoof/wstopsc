d74a9e694fe307fda218202a6194fda301e1eb54
konkahoof <147771961+konkahoof@users.noreply.github.com>
2024-10-25 00:25:32 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, redirect, url_for, make_response,render_template
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 import functools
 import hashlib
 import json
-from bs4 import BeautifulSoup
 import requests
 import re
 
@@ -35,14 +37,14 @@ def home():
         sites.append(site["site"])
     return render_template('home.html', sites=sites)
 
+#dia musavat yenicag  cf_clearance=8SFoZzwGXsViOZzXANRFQnIb_N4h328Id4TQGYUbuxs-1680443551-0-160;
+headersf = {
+    "cookie":"cf_clearance=zHQDGp79IGPynjKH6mKWCwkc9hCkYyLNKkaFLkuw.us-1682327995-0-150;YMQ2019=05e9b270802b5b41869fc4e3e26fb910;lang=az;current_lang=az",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    }
 @app.route('/res', methods=['GET'])
 def scraper():
-    #dia musavat yenicag  cf_clearance=8SFoZzwGXsViOZzXANRFQnIb_N4h328Id4TQGYUbuxs-1680443551-0-160;
-    headersf = {
-        "cookie":"cf_clearance=zHQDGp79IGPynjKH6mKWCwkc9hCkYyLNKkaFLkuw.us-1682327995-0-150;YMQ2019=05e9b270802b5b41869fc4e3e26fb910;lang=az;current_lang=az",
-        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-        }
     if 'url' in request.args and request.args.get('url') is not None:
         url = request.args.get('url')
         print(f"Requested URL: {url}")
@@ -82,6 +84,67 @@ def scraper():
                     if i == lim:
                         break
     return json.dumps(newss)
+
+def dosyaya_yaz(dosya_adi, veri):
+    with open(dosya_adi, 'w') as dosya:
+        dosya.write(veri)
+        
+def simplify_url(url):
+    parsed_url = urlparse(url)
+    simplified_url = f"{parsed_url.scheme}://{parsed_url.netloc}/"
+    return simplified_url.replace("www.","")
+def clearscr(soup):
+    # `style` ve `script` etiketlerini kaldır
+    for tag in soup(["style", "script"]):
+        tag.decompose()
+    # Tüm etiketlerdeki `style` özniteliklerini kaldır
+    for tag in soup.find_all(True):
+        if tag.has_attr("style"):
+            del tag["style"]
+    return soup
+
+def temizle_html(data,blok):
+    try:
+        for blk in blok:
+            for tag in data.select(blk):
+                if isinstance(tag, Tag):
+                    tag.decompose()
+    except:
+        blok
+    return data
+
+@app.route('/oku/<path:post>', methods=['GET'])
+def oku(post):
+    data = json.loads(haberscraper(post))
+    return render_template('oku.html', site="",haberler="", data=data)
+
+
+@app.route('/haber/<path:post>', methods=['GET'])
+def haberscraper(post):
+    print(f"Requested URL: {post}")
+    print(simplify_url(post) )
+    with open(jsonfile) as file:
+        data = json.loads(file.read())
+    for item in data:
+        if item['site'] == simplify_url(post) :
+            news={}
+            r = requests.get(post,headers=headersf)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            soup = clearscr(soup)
+            try:
+                soup = temizle_html(soup,item['adblk'])
+            except:
+                soup
+            try:
+                haber = soup.select_one(item['postblok'])
+                news["title"]=haber.select_one(item['posttitle']).text
+                news["text"]=str(haber.select_one(item['posttext'])).replace("\n","")
+            except:
+                news["title"]="Halhazırda bu kontent mövcud deyil"
+                news["text"]=str("Sayta Keçid: <a href='"+post+"'>"+post+"</a>")
+                return  json.dumps(news, indent=4, ensure_ascii=False)
+
+    return json.dumps(news, indent=4, ensure_ascii=False)
 
 
 
